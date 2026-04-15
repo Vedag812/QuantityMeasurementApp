52e64c05b2e6156246465e3a1bd7591cdcfb5bae
Vedag812 <vedag812@gmail.com>
2026-04-15 09:41:59 +0530

    Add UC3 generic length implementation (single file)

--- test/QuantityMeasurementTest.java
+++ test/QuantityMeasurementTest.java
@@ -3,68 +3,69 @@ package com.apps.quantitymeasurement;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-import com.apps.quantitymeasurement.QuantityMeasurementApp.Feet;
-import com.apps.quantitymeasurement.QuantityMeasurementApp.Inches;
+import com.apps.quantitymeasurement.QuantityMeasurementApp.Length;
+import com.apps.quantitymeasurement.QuantityMeasurementApp.LengthUnit;
 
 public class QuantityMeasurementAppTest {
 
-    // Feet Tests
     @Test
-    public void testFeetEquality_SameValue() {
-        assertEquals(new Feet(1.0), new Feet(1.0));
+    public void testFeetEquality() {
+        assertEquals(
+                new Length(1.0, LengthUnit.FEET),
+                new Length(1.0, LengthUnit.FEET)
+        );
     }
 
     @Test
-    public void testFeetEquality_DifferentValue() {
-        assertNotEquals(new Feet(1.0), new Feet(2.0));
+    public void testInchesEquality() {
+        assertEquals(
+                new Length(1.0, LengthUnit.INCHES),
+                new Length(1.0, LengthUnit.INCHES)
+        );
     }
 
     @Test
-    public void testFeetEquality_Null() {
-        assertNotEquals(new Feet(1.0), null);
+    public void testFeetInchesComparison() {
+        assertEquals(
+                new Length(1.0, LengthUnit.FEET),
+                new Length(12.0, LengthUnit.INCHES)
+        );
     }
 
     @Test
-    public void testFeetEquality_DifferentClass() {
-        assertNotEquals(new Feet(1.0), "1.0");
+    public void testFeetInequality() {
+        assertNotEquals(
+                new Length(1.0, LengthUnit.FEET),
+                new Length(2.0, LengthUnit.FEET)
+        );
     }
 
     @Test
-    public void testFeetEquality_SameReference() {
-        Feet f = new Feet(1.0);
-        assertEquals(f, f);
-    }
-
-    // Inches Tests
-    @Test
-    public void testInchesEquality_SameValue() {
-        assertEquals(new Inches(1.0), new Inches(1.0));
+    public void testInchesInequality() {
+        assertNotEquals(
+                new Length(1.0, LengthUnit.INCHES),
+                new Length(2.0, LengthUnit.INCHES)
+        );
     }
 
     @Test
-    public void testInchesEquality_DifferentValue() {
-        assertNotEquals(new Inches(1.0), new Inches(2.0));
+    public void testNullComparison() {
+        assertNotEquals(
+                new Length(1.0, LengthUnit.FEET),
+                null
+        );
     }
 
     @Test
-    public void testInchesEquality_Null() {
-        assertNotEquals(new Inches(1.0), null);
+    public void testSameReference() {
+        Length l = new Length(1.0, LengthUnit.FEET);
+        assertEquals(l, l);
     }
 
     @Test
-    public void testInchesEquality_DifferentClass() {
-        assertNotEquals(new Inches(1.0), "1.0");
-    }
-
-    @Test
-    public void testInchesEquality_SameReference() {
-        Inches i = new Inches(1.0);
-        assertEquals(i, i);
-    }
-
-    // IMPORTANT UC2 TEST
-    @Test
-    public void testFeetAndInches_NotEqual() {
-        assertNotEquals(new Feet(1.0), new Inches(1.0));
+    public void testNullUnit() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Length(1.0, null);
+        });
     }
 }
